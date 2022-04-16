dc5dcd2adfae1065ee1593d25f3b2aa93151de7f
Thaniksha <73345437+Thaniksha@users.noreply.github.com>
2022-04-16 16:26:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+mustachex=0;
+mustachey=0;
+
+function preload(){
+    mustache=loadImage('https://i.postimg.cc/BQRpD61s/pngfind-com-realistic-mustache-png-145189.png')
+}
+
+function setup(){
+    canvas=createCanvas(400,300);
+    canvas.center()
+    chitti=createCapture(VIDEO);
+    chitti.size(400,300);
+    chitti.hide();
+
+  poseNet=ml5.poseNet(chitti,modelLoaded);
+    poseNet.on('pose',getPoses);
+}
+
+
+function draw(){
+    image(chitti,0,0,400,300)
+    image(mustache,mustachex,mustachey,30,30)
+    }
+
+function getPoses(results){
+    if(results.length>0){
+        console.log(results)
+        mustachex=results[0].pose.nose.x-10;
+        mustachey=results[0].pose.nose.y-5;
+    }
+}   
+
+function modelLoaded(){
+    console.log("Posenet is initialized successfully")
+}
